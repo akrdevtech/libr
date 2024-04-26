2a955630a3690fa4b9ef2ddd09738d18c930976f
Akhil Krishnan <94969469+akrdevtech@users.noreply.github.com>
2024-04-26 07:19:58 +0000

    cors enabled

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const mongoose = require('mongoose');
 const bookRouter = require('./routes/books');
 const userRouter = require('./routes/users');
 const auth = require('./middlewares/authMiddleware');
+const cors = require('cors');
 
 const app = express();
 
@@ -18,6 +19,7 @@ mongoose.connect(process.env.MONGO_URL)
     .catch(err => console.error('Could not connect to MongoDB...', err));
 
 app.use(express.json())
+app.use(cors());
 
 app.use('/api/books', auth, bookRouter);
 app.use('/api/users', userRouter);
